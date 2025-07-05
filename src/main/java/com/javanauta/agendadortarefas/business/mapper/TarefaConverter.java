f37d11526aac6e38c8dd72d6c69a4b6ccfc44971
Adriano Abreu <adriano.luis.machado2013@hotmail.com>
2025-07-05 13:56:55 -0300

    Implementando metodos get de tarefas por email e por periodo

--- src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefaConverter.java
+++ src/main/java/com/javanauta/agendadortarefas/business/mapper/TarefaConverter.java
@@ -3,10 +3,19 @@ package com.javanauta.agendadortarefas.business.mapper;
 import com.javanauta.agendadortarefas.business.dto.TarefaDTO;
 import com.javanauta.agendadortarefas.infrastructure.entity.TarefasEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface TarefaConverter {
 
+    @Mapping(source = "id", target = "id")
+    @Mapping(source = "dataEvento", target = "dataEvento")
+    @Mapping(source = "dataCriacao", target = "dataCriacao")
     TarefasEntity paraTarefaEntity(TarefaDTO dto);
     TarefaDTO paraTarefaDTO(TarefasEntity entity);
+    List<TarefasEntity> paraListaTarefasEntity(List<TarefaDTO> dtos);
+    List<TarefaDTO> paraListaTarefasDTO(List<TarefasEntity> entities);
+
 }
--- src/main/java/com/javanauta/agendadortarefas/controller/TarefaController.java
+++ src/main/java/com/javanauta/agendadortarefas/controller/TarefaController.java
@@ -3,8 +3,12 @@ package com.javanauta.agendadortarefas.controller;
 import com.javanauta.agendadortarefas.business.TarefaService;
 import com.javanauta.agendadortarefas.business.dto.TarefaDTO;
 import lombok.RequiredArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 @RestController
 @RequestMapping("/tarefas")
@@ -18,6 +22,22 @@ public class TarefaController {
                                                    @RequestHeader("Authorization") String token){
         return ResponseEntity.ok(tarefaService.gravarTarefa(token, dto));
     }
+
+    @GetMapping("/eventos")
+    public ResponseEntity<List<TarefaDTO>> buscaListaDeTarefasPorPeriodo(
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataInicial,
+            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime dataFinal){
+
+            return ResponseEntity.ok(tarefaService.buscaTarefasAgendadasPorPeriodo(dataInicial,dataFinal));
+    }
+
+    @GetMapping
+    public ResponseEntity<List<TarefaDTO>> buscaTarefasPorEmail(@RequestHeader("Authorization") String token) {
+        return  ResponseEntity.ok(tarefaService.buscaTarefasPorEmail(token));
+    }
+
+
+
 }
 
 
